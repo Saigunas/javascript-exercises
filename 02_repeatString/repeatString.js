7059900029e510e4d08369aa207eb6709278887d
Saigunas <saigunasb777@gmail.com>
2022-02-24 15:45:43 +0200

    Completed exercises 01, 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(wrd, n) {
 
+    if(n < 0) {return "ERROR"}
+
+    let repeatedWrd = '';
+    for(let i = 0; i < n; i++) {
+        repeatedWrd += wrd;
+    }
+    console.log(repeatedWrd);
+    return repeatedWrd;
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;

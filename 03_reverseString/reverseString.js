f823ad37f094fe7d23e93615cda36cb8ff24c4b3
Saigunas <saigunasb777@gmail.com>
2022-02-24 16:34:50 +0200

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(wrd) {
+    let wrdReversed = wrd.slice(-1);
+    for(let i = 1; i < wrd.length; i++) {
+        wrdReversed += wrd.slice(-1-i, -i);
+    }
+    console.log(wrdReversed);
+    return wrdReversed;
 };
-
+reverseString('John');
 // Do not edit below this line
 module.exports = reverseString;

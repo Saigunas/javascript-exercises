9189209fa8248444e30e0eb3b6aaf56f2ab0a2ca
Saigunas <saigunasb777@gmail.com>
2022-02-25 10:25:38 +0200

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  c = (f - 32) * 0.5556;
+  c = round(c, 1);
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  f = (c * 1.8) + 32;
+  f = round(f, 1);
+  return f;
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+ftoc(32);
 
 // Do not edit below this line
 module.exports = {

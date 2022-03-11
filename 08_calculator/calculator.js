2f474e9c31868a4de538f5861503a269b27bd342
Saigunas <saigunasb777@gmail.com>
2022-03-11 09:45:46 +0200

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sumOfArr = 0;
+	for(let n of arr) {
+    sumOfArr = sumOfArr + n;
+  }
+  return sumOfArr;
+};
+const multiply = function(arr) {
+  let multi = 1;
+  for(let n of arr) {
+    multi = multi * n;
+  }
+  return multi;
 };
 
-const multiply = function() {
-
+multiply([2,3,4]);
+const power = function(n, pow) {
+  let powered = 1;
+	for(let i = 1; i <= pow; i++) {
+    powered = powered * n;
+  }
+  return powered;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	let factoring = 1;
+  for(let i = n; i > 1; i--) {
+    factoring = factoring * i;
+  }
+  return factoring;
 };
 
 // Do not edit below this line
